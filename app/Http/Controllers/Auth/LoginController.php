9acc6db89f22e93dc0b7ebc3fad69a0f6ce5e527
unknown <nanang@nanangNB.corp.garudafood.co.id>
2017-12-30 21:47:10 +0700

    login hanya untuk user yang status nya aktif

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Alumni;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -40,5 +42,50 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+        $email = $request->get($this->username());
+        $alumni = Alumni::where($this->username(), $email)->first();
+        if($alumni != null){
+            if ($alumni->status === 0) {
+                return $this->sendFailedLoginResponse($request, 'Akun anda belum diapprove, silahkan hubungi admin');
+            }
+        }
+        return $this->sendFailedLoginResponse($request, "Email/Password anda salah");
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        $credentials = $request->only($this->username(), 'password');
+        // Customization: validate if client status is active (1)
+        $credentials['status'] = 1;
+        return $credentials;
+    }
+    
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $field
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendFailedLoginResponse(Request $request, $trans = 'auth.failed')
+    {
+        $errors = [$this->username() => trans($trans)];
+        if ($request->expectsJson()) {
+            return response()->json($errors, 422);
+        }
+        return redirect()->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors($errors);
     }
 }
