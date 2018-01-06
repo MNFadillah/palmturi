b4e2f9788da3bee868140a92ec0fa1940b6e137e
Muhammad Syafriansyah <safriansah@gmail.com>
2018-01-06 16:31:00 +0700

    file web php ketinggalan

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::resource('berita', 'BeritaController');
 
 Route::post('/berita', ['as' => 'berita', 'uses' => 'BeritaController@showMore']);
 
+Route::get('berita/','BeritaController@index');
+
+Route::get('berita/{id}/more','BeritaController@more');
+
 Route::post('/saveComment', ['as' => 'saveComment', 'uses' => 'BeritaController@saveComment']);
 
 Route::get('/profil', 'ProfileController@index');
@@ -43,17 +47,32 @@ Route::get('angkatan/{id}', 'AlumniController@angkatan');
 
 Route::resource('alumni', 'AlumniController');
 
-Route::get('/adm/', 'DashboardController@index');
-
-Route::get('/adm/dashboard', 'DashboardController@index');
-
-Route::resource('/adm/alumni', 'AdmAlumniController');
-
-Route::resource('/adm/berita', 'AdmBeritaController');
-
-Route::post('/adm/berita/delete', 'AdmBeritaController@delete');
-
 Route::get('/adm/tes', function(){
-    return view('admin.editberita');
+    return view('admin.pengaturan');
 });
 
+Route::prefix('adm')->group(function() {
+    Route::get('/login','Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    
+    Route::post('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+    
+    Route::get('/', 'DashboardController@index')->name('admin.dashboard');
+
+    Route::get('/dashboard', 'DashboardController@index');
+
+    Route::get('/setting', 'DashboardController@setting');
+
+    Route::post('/setting/savepass', 'DashboardController@savepass')->name('password');
+
+    Route::post('/setting/saveprof', 'DashboardController@saveprof')->name('profile');
+
+    Route::resource('/alumni', 'AdmAlumniController');
+
+    Route::post('/alumni/approve', 'AdmAlumniController@approve')->name('approve');
+
+    Route::resource('/berita', 'AdmBeritaController');
+
+    Route::post('/berita/delete', 'AdmBeritaController@delete');
+});
